3f95c22d81704c197c729add7fb502548b546e74
LuanGranado <131273881+LuanGranado@users.noreply.github.com>
2024-03-22 14:43:31 -0300

    criaçao do evento.cs EventoController e EventoDAO

--- api/Models/Evento.cs
+++ api/Models/Evento.cs
@@ -1,6 +1,18 @@
-﻿namespace api.Repository;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Repository;
 
 public class Evento
 {
-    
+    [Column("id")]
+        public int Id  { get; set; }
+
+         [Column("data_evento")]
+        public DateTime DataEvento { get; set; }
+
+        [Column("total_ingressos")]
+        public int TotalIngressos {get; set;}
+
+        [Column("descricao")]
+        public string? Descricao {get; set;}
 }
